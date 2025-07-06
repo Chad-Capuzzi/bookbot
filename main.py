4e32313e4bced4e1e98680abc669a4e326d9c87e
Chad-Capuzzi <capuzzichad12@gmail.com>
2025-07-06 12:37:44 -0400

    initialize a dictionary of characters with count 0

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, char_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,6 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
-    print(f"{num_words} words found in the document")
-
+    #print(f"{num_words} words found in the document")
+    print(char_count(book_text))
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,9 @@
 def count_words(book_text):
     return len(book_text.split())
+
+def char_count(book_text):
+    char_dict = {}
+    for c in book_text:
+        if c.lower() not in char_dict:
+            char_dict[c.lower()] = 0
+    return char_dict
