da368a23e5ec67ea668cbac219a0a051517af36a
Chad-Capuzzi <capuzzichad12@gmail.com>
2025-07-08 10:54:27 -0400

    book path no longer hard coded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, char_count, sorted_chars
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,14 +7,18 @@ def get_book_text(filepath):
     
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exti(1)
+
+    book_text = get_book_text(sys.argv[1])
     num_words = count_words(book_text)
     characters = char_count(book_text)
     sorted_list = sorted_chars(characters)
     #print(f"{num_words} words found in the document")
     print(f"""
 ============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------
 Found {num_words} total words
 --------- Character Count -------
